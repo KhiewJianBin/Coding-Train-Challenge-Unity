ba55a6d1f69785a702c62ab3e0da0e38a92c62d3
frozonnorth <khiewbin@gmail.com>
2019-06-03 18:26:11 +0800

    Coding Challenge #1: Starfield in Processing

---
+++ Assets/Extension.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Extension : MonoBehaviour
+{
+    static public float Map
+        (float value,
+        float start1,float stop1,
+        float start2,float stop2)
+    {
+        return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+    }
+}
